49d5750b06871fa664bb38c2dc57995e719c24f5
naxifa <maliha1@usf.edu>
2024-09-17 17:37:34 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,13 @@
+
+// Task 1- Creating an Inventory Array of Product Objects
+
+const Inventory =  [
+    { Name: "Coffee Beans", Price: 5, Quantity: 100 },
+    { Name: "Milk", Price: 4, Quantity: 90},
+    { Name: "Sugar", Price: 3, Quantity: 75 },
+    { Name: "Cups", Price: 2, Quantity: 150 },
+    ];
+
+// To display array of inventory
+console.log(Inventory);
+
