8873aa0a269451750ebc4c8b1cc823cf150f0083
naxifa <maliha1@usf.edu>
2024-09-17 17:54:59 -0400

    Initialize orders array

--- order_management_system.js
+++ order_management_system.js
@@ -1,13 +1,20 @@
 
 // Task 1- Creating an Inventory Array of Product Objects
 
-const Inventory =  [
-    { Name: "Coffee Beans", Price: 5, Quantity: 100 },
-    { Name: "Milk", Price: 4, Quantity: 90},
-    { Name: "Sugar", Price: 3, Quantity: 75 },
-    { Name: "Cups", Price: 2, Quantity: 150 },
+const inventory =  [
+    { name: "Espresso", price: 5, quantity: 20 },
+    { name: "Latte", price: 4, quantity: 50},
+    { name: "Cappuccino", price: 4, quantity: 55 },
+    { name: "Mocha", price: 5, quantity: 45 },
     ];
 
 // To display array of inventory
-console.log(Inventory);
+console.log(inventory);
+
+
+// Task 2- Creating an Orders Array of Order Objects
+
+const orders = []; // Empty orders array
+
+
 
